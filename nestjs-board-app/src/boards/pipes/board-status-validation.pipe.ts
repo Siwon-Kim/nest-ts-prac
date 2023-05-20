67d355f3df68dac3b0ed591a5d85852e066a87d6
Siwon-Kim <siwonkim1108@gmail.com>
2023-05-20 21:28:09 +0900

    feat: body data validation using pipe

--- nestjs-board-app/src/boards/pipes/board-status-validation.pipe.ts
+++ nestjs-board-app/src/boards/pipes/board-status-validation.pipe.ts
@@ -1,7 +1,19 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../boards.model';
 
 export class BoardStatusValidationPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} isn't in the status options`);
+    }
     return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
   }
 }
