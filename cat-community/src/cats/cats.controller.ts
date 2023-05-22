a114e26283d5a0bce6a17bd7cec194c35e451ca9
Siwon-Kim <siwonkim1108@gmail.com>
2023-05-22 14:50:09 +0900

    feat: exception filter - errorHandler

--- cat-community/src/cats/cats.controller.ts
+++ cat-community/src/cats/cats.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpException } from '@nestjs/common';
+import { Get, UseFilters } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from '../http-exception.filter';
 
 @Controller('cats')
-export class CatsController {}
+@UseFilters(HttpExceptionFilter)
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCats() {
+    throw new HttpException('get api broken', 401);
+  }
+}
